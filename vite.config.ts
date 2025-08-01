a5212eef9bdf7f00aaab5e74eb09f0c13089f50a
Daniel Hicks <danielehicks89@gmail.com>
2025-08-01 11:04:14 -0400

    updated rollupOptions

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 const mode = process.env.NODE_ENV || "development";
+import { resolve } from "path";
 
 export default defineConfig({
   base: mode === "development" ? "/" : "/staticportfolio/",
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        "404": resolve(__dirname, "public/404.html"),
+      },
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
