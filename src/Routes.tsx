e8902b714ae0060933119792947888de2a6f73c8
Daniel Hicks <danielehicks89@gmail.com>
2025-08-01 11:55:36 -0400

    last one?

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -5,33 +5,30 @@ import Designs from "./components/Designs";
 import Playground from "./components/Playground";
 import App from "./App";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        {
-          path: "/",
-          element: <HomePage />,
-        },
-        {
-          path: "/theworld",
-          element: <GlobeContainer />,
-        },
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/theworld",
+        element: <GlobeContainer />,
+      },
 
-        {
-          path: "/design",
-          element: <Designs />,
-        },
-        {
-          path: "/playground",
-          element: <Playground />,
-        },
-      ],
-    },
-  ],
-  { basename: "/staticportfolio" },
-);
+      {
+        path: "/design",
+        element: <Designs />,
+      },
+      {
+        path: "/playground",
+        element: <Playground />,
+      },
+    ],
+  },
+]);
 
 export default router;
